9b6833f4364ef1d3ce2d29dc7871ec67302af377
Jezael Andrés Ropero - 1151920 <jezael.calderon@gmail.com>
2022-09-30 11:03:33 -0500

    Se Agregó Getters/Setters

--- src/Model/Feligres.java
+++ src/Model/Feligres.java
@@ -9,11 +9,71 @@ private String nombre;
 private String direccion;
 private String telefono;
 private byte estrato;
+private String estado;
 private double diezmo;
 
+    public Feligres() {
+    }
 
-//Puede ser Deudor y Cumplid
-//private Estado estado;
+    public Feligres(String cedula, String nombre, String direccion, String telefono, byte estrato, String estado, double diezmo) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.estrato = estrato;
+        this.estado = estado;
+        this.diezmo = diezmo;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public byte getEstrato() {
+        return estrato;
+    }
+    public void setEstrato(byte estrato) {
+        this.estrato = estrato;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public double getDiezmo() {
+        return diezmo;
+    }
+    public void setDiezmo(double diezmo) {
+        this.diezmo = diezmo;
+    }
+
 
 
 
