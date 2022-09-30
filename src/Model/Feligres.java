6db1b6b38e44e2925371b33ff35f65ff01d40acc
Kevin1151912 <100525817+Kevin1151912@users.noreply.github.com>
2022-09-30 11:59:36 -0500

    se agregego el equals y un poco de consultarDiezmo

--- src/Model/Feligres.java
+++ src/Model/Feligres.java
@@ -1,6 +1,8 @@
 
 package Model;
 
+import java.util.Objects;
+
 
 public class Feligres {
     
@@ -8,14 +10,14 @@ private String cedula;
 private String nombre;
 private String direccion;
 private String telefono;
-private byte estrato;
+private double estrato;
 private String estado;
 private double diezmo;
 
     public Feligres() {
     }
 
-    public Feligres(String cedula, String nombre, String direccion, String telefono, byte estrato, String estado, double diezmo) {
+    public Feligres(String cedula, String nombre, String direccion, String telefono, double estrato, String estado, double diezmo) {
         this.cedula = cedula;
         this.nombre = nombre;
         this.direccion = direccion;
@@ -53,10 +55,10 @@ private double diezmo;
         this.telefono = telefono;
     }
 
-    public byte getEstrato() {
+    public double getEstrato() {
         return estrato;
     }
-    public void setEstrato(byte estrato) {
+    public void setEstrato(double estrato) {
         this.estrato = estrato;
     }
 
@@ -74,8 +76,48 @@ private double diezmo;
         this.diezmo = diezmo;
     }
 
-//public void asignarDiezmo(String){
-    /*hola*/
+    @Override
+    public String toString() {
+        return "Feligres{" + "cedula=" + cedula + ", nombre=" + nombre + ", direccion=" + direccion + ", telefono=" + telefono + ", estrato=" + estrato + ", estado=" + estado + ", diezmo=" + diezmo + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Feligres f = (Feligres) obj;
+		
+        if(this.getCedula().equals(f.getCedula()) && this.getNombre().equals(f.getNombre()) && this.getDireccion().equals(f.getDireccion()) && this.getTelefono().equals(f.getTelefono()) && 
+                this.getEstrato()== f.getEstrato() && this.getEstado().equals(f.getEstado()) && this.getDiezmo()== f.getDiezmo() ) {
+            return true;
+        }
+        return false;
+    }
+    
+    
+
+    public void asignarDiezmo(){
+        if (estrato==1){
+            diezmo=250000;
+        }
+        else if(estrato==2 || estrato==3){
+            diezmo=500000;
+        }
+        else if(estrato>3){
+            diezmo=1000000;
+        }
+        else{
+            System.out.print("El estrato tiene que ser superior a 0");
+        }
+    }
+    
+    public void consultarDiezmo(String cedula){
+        
+    }
 
 
 }
