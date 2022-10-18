4f57fd0c5abc675496d24026d379bf6b8792ebcf
Kevin1151912 <100525817+Kevin1151912@users.noreply.github.com>
2022-10-18 10:03:18 -0500

    Agregar ejercicio Parroquia Casi terminado

--- src/Control/Control.java
+++ src/Control/Control.java
@@ -4,31 +4,108 @@
  */
 package Control;
 
+import Model.Feligres;
+import Model.Iglesia;
+import Vista.Vista;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import vista.Vista;
-/**
- *
- * @author kevin
- */
+import java.awt.event.ActionEvent;
+import javafx.fxml.FXML;
 
-/**
- *
- * @author estudiante
- */
-public class Control {
 
-    Vista vista = new Vista();
+public class Control implements ActionListener{
+    private Vista vista;
+    private Feligres f;
+    private Iglesia i;
 
-    ActionListener al = new ActionListener() {
-        public void actionPerformed() {
-            System.out.println("Hola");
+    public Control(Vista vista, Feligres f, Iglesia i) {
+        this.vista = vista;
+        this.f = f;
+        this.i = i;
+        ActionListener(this);
+    }
+    
+    void jbGuardar(ActionEvent e) {
+    	f.setCedula(vista.txtCedula.getText());
+        f.setNombre(vista.txtNombre.getText());
+        f.setDireccion(vista.txtDireccion.getText());
+        f.setTelefono(vista.txtTelefono.getText());
+        f.setEstrato(vista.txtEstrato.getText());
+        f.setEstado(vista.txtEstado.getText());
+        f= new Feligres(f.getCedula(), f.getNombre(), f.getDireccion(), f.getTelefono(), f.getEstrato(), f.getEstado());
+        i.agregarFeligres(f);
+        System.out.println("----------");
+    }
+    
+    /*@FXML
+    void jbBuscar(ActionEvent e) {
+    	Feligres feli= i.buscarFeligres(vista.txtCedula.getText());
+            vista.txtNombre.setText(feli.getNombre());
+            vista.txtTelefono.setText(feli.getTelefono());
+            vista.txtEstrato.setText(feli.getEstrato());
+            vista.txtEstado.setText(feli.getEstado());
+            vista.txtDireccion.setText(feli.getDireccion());
+    }*/
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("Guardar")){
+        f.setCedula(vista.txtCedula.getText());
+        f.setNombre(vista.txtNombre.getText());
+        f.setDireccion(vista.txtDireccion.getText());
+        f.setTelefono(vista.txtTelefono.getText());
+        f.setEstrato(vista.txtEstrato.getText());
+        f.setEstado(vista.txtEstado.getText());
+        Feligres fe= new Feligres(f.getCedula(), f.getNombre(), f.getDireccion(), f.getTelefono(), f.getEstrato(), f.getEstado());
+        i.agregarFeligres(fe);
+        System.out.println("Guardado");
         }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(e.getActionCommand().equals("Buscar")){
+            Feligres feli= i.buscarFeligres(vista.txtCedula.getText());
+            vista.txtNombre.setText(feli.getNombre());
+            vista.txtTelefono.setText(feli.getTelefono());
+            vista.txtEstrato.setText(feli.getEstrato());
+            vista.txtEstado.setText(feli.getEstado());
+            vista.txtDireccion.setText(feli.getDireccion());
+            
         }
-    };
+        if(e.getActionCommand().equals("Consultar")){
+            Feligres feli= i.buscarFeligres(vista.txtCedula1.getText());
+            String diezmo= String.valueOf(feli.getDiezmo());
+            vista.txtValorDiesmo.setText(diezmo);
+            
+        }
+        if(e.getActionCommand().equals("Pagar")){
+            Feligres feli=i.buscarFeligres(vista.txtCedula1.getText());
+            vista.jTextArea1.setText(feli.pagarDiezmo());
+        }
+        if(e.getActionCommand().equals("Totalizar")){
+            vista.jTextArea1.setText(f.totalizar(vista.txtCedula1.getText()));
+        }
+        if(e.getActionCommand().equals("Eliminar")){
+            i.eliminarFeligres(vista.txtCedula.getText());
+        }
+        if(e.getActionCommand().equals("Actualizar")){
+            Feligres feli= i.buscarFeligres(vista.txtCedula.getText());
+            String cedula=vista.txtCedula.getText();
+            String nombre=vista.txtNombre.getText();
+            String direccion=vista.txtDireccion.getText();
+            String telefono=vista.txtTelefono.getText();
+            String estrato=vista.txtEstrato.getText();
+            String estado=vista.txtEstado.getText();
+            feli.actualizarDatos(cedula, nombre, direccion, telefono, estrato, estado);
+            
+        }
+    }
+      
 
+    private void ActionListener(Control control) {
+        vista.jbGuardar.addActionListener(control);
+        vista.jbBuscar.addActionListener(control);
+        vista.jbConsultar.addActionListener(control);
+        vista.jbTotalizar.addActionListener(control);
+        vista.jbPagar.addActionListener(control);
+        vista.jbEliminar.addActionListener(control);
+        vista.jbActualizar.addActionListener(control);
+    } 
 }
